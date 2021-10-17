853510a110b08b02f4f6b42ed1991ee2b67d0c0f
gentlekboy <gentlekboy@gmail.com>
2021-10-17 22:26:36 +0100

    created form component with state and props

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,18 +1,46 @@
-import React from "react";
+import React, { FormEvent } from "react";
+import { useState, FC } from 'react';
+import Profile from "./Profile";
+import '../styles/form.css'
+import { OnchangeInterface } from "../interfaces";
 
-function Form() {
+const Form: FC<{}> = (props) =>  {
+    const [submitted, setSubmitted] = useState(false)
+
+    const [user, setUser] = useState({
+        firstName: "",
+        lastName: "",
+        email: "",
+        tel: "",
+        age: ""
+    });
+
+    const {firstName, lastName, email, tel, age} = user;
+
+    const onChange = (e:OnchangeInterface) => {
+        setUser({...user, [e.target.name]: e.target.value})
+    }
+
+    const onSubmit = (e:FormEvent)  => {
+        e.preventDefault();
+        setSubmitted(true)
+    }
+
     return (
-        <form>
-            <h1>Register</h1>
+        submitted ? <Profile {...user} /> :
+        <div className="formContainer">
+            <form onSubmit={onSubmit}>
+                <h1>Register</h1>
 
-            <input type="text" name="firstName" id="firstName" placeholder="Enter your first name here" /><br />
-            <input type="text" name="lastName" id="lastName" placeholder="Enter your last name here" /><br />
-            <input type="email" name="email" id="email" placeholder="Enter your email here"/><br />
-            <input type="tel" name="tel" id="tel" placeholder="Enter your mobile number here"/><br />
-            <input type="number" name="age" id="age" placeholder="Enter your age here"/><br />
+                <input type="text" name="firstName" id="firstName" placeholder="First name here" value={firstName} onChange={onChange}/><br />
+                <input type="text" name="lastName" id="lastName" placeholder="Last name here" value={lastName} onChange={onChange} /><br />
+                <input type="email" name="email" id="email" placeholder="Email here" value={email} onChange={onChange}/><br />
+                <input type="tel" name="tel" id="tel" placeholder="Mobile number here" value={tel} onChange={onChange}/><br />
+                <input type="number" name="age" id="age" placeholder="Age here" value={age} onChange={onChange}/><br />
 
-            <button type="submit">Save</button>
-        </form>
+                <button type="submit">Save</button>
+            </form>
+        </div>
     )
 }
 
