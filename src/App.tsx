751cbe6697e7553585629928f16417d58289d042
gentlekboy <gentlekboy@gmail.com>
2021-10-17 22:25:33 +0100

    added router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
 import './App.css';
 import Form from './components/Form';
+import {BrowserRouter as Router} from 'react-router-dom'
 
 function App() {
   return (
-    <Form/>
+    <Router>
+          <Form/>
+    </Router>
   );
 }
 
