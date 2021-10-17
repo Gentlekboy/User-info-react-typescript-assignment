11086b573497d961eacafb75653f1e712f67de93
gentlekboy <gentlekboy@gmail.com>
2021-10-17 22:25:48 +0100

    created interfaces

---
+++ src/interfaces.ts
@@ -0,0 +1,11 @@
+export interface OnchangeInterface{
+    target:{ name: string; value: string}
+}
+
+export interface PropTypes{
+    firstName:string,
+    lastName: string,
+    tel: string,
+    email: string,
+    age: string
+}
